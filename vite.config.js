ef85f82963e343cccd76d82d34b919a577a2a3dd
asgoDev <asgodev@gmail.com>
2025-06-17 18:29:52 -0400

    vite.config.updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'https://asgoDev.github.io/', // Esto es CRÍTICO para GitHub Pages
+  base: 'https://asgoDev.github.io/UE-Cristo-Rey-Maestro', // Esto es CRÍTICO para GitHub Pages
   build: {
     outDir: 'dist',
     emptyOutDir: true,
